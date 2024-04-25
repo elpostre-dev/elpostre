4d15ddd0fd4453ff4682cf355825901e1d397747
Francisco Guerra <paco.gq.0709@gmail.com>
2024-04-24 18:53:59 -0600

    optimize images, product card and details page

--- components/ProductCard.tsx
+++ components/ProductCard.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Producto } from "@/data/productos"
 import Link from 'next/link';
+import Image from 'next/image';
 
 interface ProductCardProps {
     producto: Producto;
@@ -13,15 +14,17 @@ const ProductCard: React.FC<ProductCardProps> = ({ producto }) => {
 
     return (
         <div className="flex flex-col xl:w-64 lg:w-72 md:w-80 w-80 bg-white shadow-md rounded-xl duration-500 hover:scale-105 hover:shadow-xl" key={producto.id}>
-            <Link href={productURL} className="flex flex-col h-full">
-
+            <Link href={productURL} className="flex h-full flex-col">
                 {/* foto */}
-                <img src={producto.fotos[0]}
+                <Image
+                    src={producto.fotos[0]}
                     alt={producto.nombre}
                     className="xl:h-72 h-80 xl:w-64 lg:w-72 md:w-80 w-80 object-cover rounded-t-xl"
+                    width={200}
+                    height={200}
                 />
 
-                <div className="px-4 py-3 flex flex-col flex-grow xl:w-64 lg:w-72 md:w-80 w-80">
+                <div className="px-4 py-3 flex flex-col flex-grow xl:w-64 lg:w-72 md:w-80 w-80 text-center">
 
                     {/* categoría */}
                     <p className="text-gray-400 mr-3 uppercase text-xs">{producto.categoriaNombre}</p>
@@ -29,23 +32,30 @@ const ProductCard: React.FC<ProductCardProps> = ({ producto }) => {
                     {/* nombre */}
                     <p className="text-lg font-bold text-black block capitalize">{producto.nombre}</p>
 
-                    <hr className="my-2" />
 
                     {/* variaciones */}
-                    <div className="flex-grow">
+                    {/* <div className="flex-grow">
                         <p>
                             {producto.variaciones.map((vari) => (
-                                <span key={vari.tamanio} className="bg-gray-100 text-gray-800 text-xs font-medium mr-2 px-2.5 py-0.5 rounded">
+                                <span key={vari.tamanio} className="bg-gray-200 text-gray-800 text-xs font-medium mr-2 px-2.5 py-0.5 rounded">
                                     {vari.tamanio}
                                 </span>
                             ))}
                         </p>
+                    </div> */}
+                    <div className="flex-grow" />
+                    <hr className="my-2" />
+                    <div>
+                        <p>
+                            <span className="bg-gray-200 text-gray-800 text-xs font-medium mr-2 px-2.5 py-0.5 rounded">
+                                {producto.variaciones.length} {producto.variaciones.length > 1 ? "variaciones" : "variación"}
+                            </span>
+                        </p>
                     </div>
-
-                    {/* Aquí puede ir la sección de precios o cualquier otro contenido adicional */}
                 </div>
             </Link>
         </div>
+
     )
 }
 
