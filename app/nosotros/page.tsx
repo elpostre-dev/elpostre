b10703a26217c61856216ed313cb2c0f95a9b895
Francisco Guerra <paco.gq.0709@gmail.com>
2024-04-25 23:10:47 -0600

    content change

--- app/nosotros/page.tsx
+++ app/nosotros/page.tsx
@@ -19,7 +19,17 @@ export default function Nosotros() {
 
                     {/* Left Side */}
                     <div className="flex flex-col justify-center items-center px-7 py-10 md:w-1/2">
-                        <Image src={'/logos/logo_dorado.png'} alt="logo" height={180} width={360} />
+                        <div className="w-full flex justify-center py-4">
+                            <div className="w-1/2 sm:w-1/2 md:w-1/2">
+                                <Image
+                                    src="/logos/logo_dorado_2.png"
+                                    alt="logo"
+                                    width={720}  // Tamaño original del logo
+                                    height={360}
+                                    layout="responsive"
+                                />
+                            </div>
+                        </div>
 
                         {/* <h1 className="text-5xl text-mainRojo-100 font-bold mb-4 text-center">
                             Pastelería El Postre
@@ -41,6 +51,8 @@ export default function Nosotros() {
                         <div className="absolute inset-0 bg-cover bg-center" style={{ backgroundImage: 'url(/sucursal.jpg)' }}></div>
                     </div>
 
+
+
                 </div>
             </div>
 
@@ -58,34 +70,8 @@ export default function Nosotros() {
                             En Pastelería El Postre, nuestro compromiso con la calidad y nuestra amplia experiencia nos distinguen. Utilizamos solo los mejores ingredientes, y cada postre es elaborado con técnicas perfeccionadas a lo largo de más de veinte años en el sector, asegurando un sabor excepcional en cada bocado.
                         </p> */}
                     </div>
-                    {/* <div className="mx-auto mt-16 max-w-2xl sm:mt-20 lg:mt-24 lg:max-w-4xl">
-                        <dl className="grid max-w-xl grid-cols-1 gap-x-8 gap-y-10 lg:max-w-none lg:grid-cols-2 lg:gap-y-16">
-                            {features.map((feature) => (
-                                <div key={feature.name} className="relative pl-16">
-                                    <dt className="text-base font-semibold leading-7 text-gray-900">
-                                        <div className="absolute left-0 top-0 flex h-10 w-10 items-center justify-center rounded-lg bg-mainRojo-100">
-                                            <svg className="h-6 w-6 text-white" aria-hidden="true" viewBox="0 0 20 20" fill="currentColor">
-                                                <path fillRule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zm0-2a6 6 0 100-12 6 6 0 000 12zm0-10a2 2 0 11-4 0 2 2 0 014 0z" clipRule="evenodd" />
-                                            </svg>
-                                        </div>
-                                        {feature.name}
-                                    </dt>
-                                    <dd className="mt-2 text-base leading-7 text-gray-600">{feature.description}</dd>
-                                </div>
-                            ))}
-                        </dl>
-                    </div> */}
 
                     <div className="grid lg:grid-cols-3 md:grid-cols-2 gap-12 max-md:max-w-lg mx-auto py-10">
-
-                        <div className="rounded-xl group p-8 text-center text-mainRojo-100 hover:bg-mainRojo-100 hover:text-white shadow-xl hover:cursor-pointer transition duration-300">
-                            <svg xmlns="http://www.w3.org/2000/svg" fill="currentColor" className="w-8 mb-4 inline-block" viewBox="0 0 32 32">
-                                <path d="M28.068 12h-.128a.934.934 0 0 1-.864-.6.924.924 0 0 1 .2-1.01l.091-.091a2.938 2.938 0 0 0 0-4.147l-1.511-1.51a2.935 2.935 0 0 0-4.146 0l-.091.091A.956.956 0 0 1 20 4.061v-.129A2.935 2.935 0 0 0 17.068 1h-2.136A2.935 2.935 0 0 0 12 3.932v.129a.956.956 0 0 1-1.614.668l-.086-.091a2.935 2.935 0 0 0-4.146 0l-1.516 1.51a2.938 2.938 0 0 0 0 4.147l.091.091a.935.935 0 0 1 .185 1.035.924.924 0 0 1-.854.579h-.128A2.935 2.935 0 0 0 1 14.932v2.136A2.935 2.935 0 0 0 3.932 20h.128a.934.934 0 0 1 .864.6.924.924 0 0 1-.2 1.01l-.091.091a2.938 2.938 0 0 0 0 4.147l1.51 1.509a2.934 2.934 0 0 0 4.147 0l.091-.091a.936.936 0 0 1 1.035-.185.922.922 0 0 1 .579.853v.129A2.935 2.935 0 0 0 14.932 31h2.136A2.935 2.935 0 0 0 20 28.068v-.129a.956.956 0 0 1 1.614-.668l.091.091a2.935 2.935 0 0 0 4.146 0l1.511-1.509a2.938 2.938 0 0 0 0-4.147l-.091-.091a.935.935 0 0 1-.185-1.035.924.924 0 0 1 .854-.58h.128A2.935 2.935 0 0 0 31 17.068v-2.136A2.935 2.935 0 0 0 28.068 12ZM29 17.068a.933.933 0 0 1-.932.932h-.128a2.956 2.956 0 0 0-2.083 5.028l.09.091a.934.934 0 0 1 0 1.319l-1.511 1.509a.932.932 0 0 1-1.318 0l-.09-.091A2.957 2.957 0 0 0 18 27.939v.129a.933.933 0 0 1-.932.932h-2.136a.933.933 0 0 1-.932-.932v-.129a2.951 2.951 0 0 0-5.028-2.082l-.091.091a.934.934 0 0 1-1.318 0l-1.51-1.509a.934.934 0 0 1 0-1.319l.091-.091A2.956 2.956 0 0 0 4.06 18h-.128A.933.933 0 0 1 3 17.068v-2.136A.933.933 0 0 1 3.932 14h.128a2.956 2.956 0 0 0 2.083-5.028l-.09-.091a.933.933 0 0 1 0-1.318l1.51-1.511a.932.932 0 0 1 1.318 0l.09.091A2.957 2.957 0 0 0 14 4.061v-.129A.933.933 0 0 1 14.932 3h2.136a.933.933 0 0 1 .932.932v.129a2.956 2.956 0 0 0 5.028 2.082l.091-.091a.932.932 0 0 1 1.318 0l1.51 1.511a.933.933 0 0 1 0 1.318l-.091.091A2.956 2.956 0 0 0 27.94 14h.128a.933.933 0 0 1 .932.932Z" data-original="#000000" />
-                                <path d="M16 9a7 7 0 1 0 7 7 7.008 7.008 0 0 0-7-7Zm0 12a5 5 0 1 1 5-5 5.006 5.006 0 0 1-5 5Z" data-original="#000000" />
-                            </svg>
-                            <h3 className="text-xl font-semibold mb-2">Customization</h3>
-                            <p className="text-gray-600 group-hover:text-gray-200 text-sm">Tailor our product to suit your needs.</p>
-                        </div>
 
                         <div className="rounded-xl group p-8 text-center text-mainRojo-100 hover:bg-mainRojo-100 hover:text-white shadow-xl hover:cursor-pointer transition duration-300">
                             <svg xmlns="http://www.w3.org/2000/svg" fill="currentColor" className="w-8 mb-4 inline-block" viewBox="0 0 682.667 682.667">
@@ -99,19 +85,34 @@ export default function Nosotros() {
                                     <path d="M178 271.894 233.894 216 334 316.105" data-original="#000000" />
                                 </g>
                             </svg>
-                            <h3 className="text-xl font-semibold mb-2">Security</h3>
-                            <p className="text-gray-600 group-hover:text-gray-200 text-sm">Your data is protected by the latest security measures.</p>
+                            <h3 className="text-xl font-semibold mb-2">Calidad</h3>
+                            <p className="text-gray-600 group-hover:text-gray-200 text-sm">
+                                Ingredientes de alta calidad para postres excepcionales.
+                            </p>
                         </div>
 
                         <div className="rounded-xl group p-8 text-center text-mainRojo-100 hover:bg-mainRojo-100 hover:text-white shadow-xl hover:cursor-pointer transition duration-300">
-                            <svg xmlns="http://www.w3.org/2000/svg" fill="currentColor" className="w-8 mb-4 inline-block" viewBox="0 0 512.001 512.001">
-                                <path d="M271.029 0c-33.091 0-61 27.909-61 61s27.909 61 61 61 60-27.909 60-61-26.909-61-60-61zm66.592 122c-16.485 18.279-40.096 30-66.592 30-26.496 0-51.107-11.721-67.592-30-14.392 15.959-23.408 36.866-23.408 60v15c0 8.291 6.709 15 15 15h151c8.291 0 15-6.709 15-15v-15c0-23.134-9.016-44.041-23.408-60zM144.946 460.404 68.505 307.149c-7.381-14.799-25.345-20.834-40.162-13.493l-19.979 9.897c-7.439 3.689-10.466 12.73-6.753 20.156l90 180c3.701 7.423 12.704 10.377 20.083 6.738l19.722-9.771c14.875-7.368 20.938-25.417 13.53-40.272zM499.73 247.7c-12.301-9-29.401-7.2-39.6 3.9l-82 100.8c-5.7 6-16.5 9.6-22.2 9.6h-69.901c-8.401 0-15-6.599-15-15s6.599-15 15-15h60c16.5 0 30-13.5 30-30s-13.5-30-30-30h-78.6c-7.476 0-11.204-4.741-17.1-9.901-23.209-20.885-57.949-30.947-93.119-22.795-19.528 4.526-32.697 12.415-46.053 22.993l-.445-.361-21.696 19.094L174.28 452h171.749c28.2 0 55.201-13.5 72.001-36l87.999-126c9.9-13.201 7.2-32.399-6.299-42.3z" data-original="#000000" />
+                            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" className="w-8 mb-4 inline-block">
+                                <path stroke-linecap="round" stroke-linejoin="round" d="m15 11.25 1.5 1.5.75-.75V8.758l2.276-.61a3 3 0 1 0-3.675-3.675l-.61 2.277H12l-.75.75 1.5 1.5M15 11.25l-8.47 8.47c-.34.34-.8.53-1.28.53s-.94.19-1.28.53l-.97.97-.75-.75.97-.97c.34-.34.53-.8.53-1.28s.19-.94.53-1.28L12.75 9M15 11.25 12.75 9" />
                             </svg>
-                            <h3 className="text-xl font-semibold mb-2">Support</h3>
-                            <p className="text-gray-600 group-hover:text-gray-200 text-sm">24/7 customer support for all your inquiries.</p>
+                            <h3 className="text-xl font-semibold mb-2">Sabor</h3>
+                            <p className="text-gray-600 group-hover:text-gray-200 text-sm">
+                                Mantenemos el auténtico sabor de los postres caseros que nos caracteriza.
+                            </p>
                         </div>
 
                         <div className="rounded-xl group p-8 text-center text-mainRojo-100 hover:bg-mainRojo-100 hover:text-white shadow-xl hover:cursor-pointer transition duration-300">
+                            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" className="w-8 mb-4 inline-block">
+                                <path stroke-linecap="round" stroke-linejoin="round" d="M6 6.878V6a2.25 2.25 0 0 1 2.25-2.25h7.5A2.25 2.25 0 0 1 18 6v.878m-12 0c.235-.083.487-.128.75-.128h10.5c.263 0 .515.045.75.128m-12 0A2.25 2.25 0 0 0 4.5 9v.878m13.5-3A2.25 2.25 0 0 1 19.5 9v.878m0 0a2.246 2.246 0 0 0-.75-.128H5.25c-.263 0-.515.045-.75.128m15 0A2.25 2.25 0 0 1 21 12v6a2.25 2.25 0 0 1-2.25 2.25H5.25A2.25 2.25 0 0 1 3 18v-6c0-.98.626-1.813 1.5-2.122" />
+                            </svg>
+
+                            <h3 className="text-xl font-semibold mb-2">Variedad</h3>
+                            <p className="text-gray-600 group-hover:text-gray-200 text-sm">
+                                Amplia selección de pasteles, galletas y brownies, incluida la línea KETO.
+                            </p>
+                        </div>
+
+                        {/* <div className="rounded-xl group p-8 text-center text-mainRojo-100 hover:bg-mainRojo-100 hover:text-white shadow-xl hover:cursor-pointer transition duration-300">
                             <svg xmlns="http://www.w3.org/2000/svg" fill="currentColor" className="w-8 mb-4 inline-block" viewBox="0 0 24 24">
                                 <g fill-rule="evenodd" clip-rule="evenodd">
                                     <path d="M17.03 8.97a.75.75 0 0 1 0 1.06l-4.2 4.2a.75.75 0 0 1-1.154-.114l-1.093-1.639L8.03 15.03a.75.75 0 0 1-1.06-1.06l3.2-3.2a.75.75 0 0 1 1.154.114l1.093 1.639L15.97 8.97a.75.75 0 0 1 1.06 0z" data-original="#000000" />
@@ -150,7 +151,7 @@ export default function Nosotros() {
                             </svg>
                             <h3 className="text-xl font-semibold mb-2">Communication</h3>
                             <p className="text-gray-600 group-hover:text-gray-200 text-sm">Seamless communication for your team.</p>
-                        </div>
+                        </div> */}
 
                     </div>
 
@@ -166,6 +167,6 @@ export default function Nosotros() {
             <Footer />
 
 
-        </main>
+        </main >
     );
 }
