8a56a8e66f4f83cc20d4e510b62e3f88663f09ca
Francisco Guerra <paco.gq.0709@gmail.com>
2024-04-28 15:34:28 -0600

    navbar animations

--- components/NavLink.tsx
+++ components/NavLink.tsx
@@ -13,7 +13,7 @@ export default function NavLink({
             <Link
                 href={href}
                 className={clsx(
-                    'block py-2 px-3 rounded text-lg md:rounded-none hover:text-mainRojo-100 md:p-0 border-b-2 border-transparent md:hover:border-b-2 md:hover:border-mainRojo-100',
+                    'block py-2 px-3 rounded text-lg md:rounded-none hover:text-mainRojo-100 md:p-0 border-b-2 border-transparent md:hover:border-b-2 link-underline-md relative',
                     {
                         'text-mainRojo-100 bg-mainAzul-100 md:bg-transparent md:text-mainRojo-100': path == href,
                         'text-gray-700 hover:bg-gray-100 md:hover:bg-transparent': path != href,
--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -23,7 +23,6 @@ const links = [
 ];
 
 export default function NavBar() {
-
     const [isMenuOpen, setIsMenuOpen] = useState(false);
 
     const toggleMenu = () => {
@@ -42,14 +41,19 @@ export default function NavBar() {
                 <button
                     onClick={toggleMenu}
                     type="button"
-                    className="inline-flex items-center p-2 h-10 justify-center text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200" aria-controls="navbar-default" aria-expanded={isMenuOpen}
+                    className="inline-flex items-center p-2 h-10 justify-center text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200"
+                    aria-controls="navbar-default" aria-expanded={isMenuOpen}
                 >
                     <span className="sr-only">Open main menu</span>
-                    <svg className="h-5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 17 14">
+                    <svg className="h-5 w-5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 17 14">
                         <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M1 1h15M1 7h15M1 13h15" />
                     </svg>
                 </button>
-                <div className={`w-full md:block md:w-auto ${isMenuOpen ? '' : 'hidden'}`} id="navbar-default">
+                <div
+                    className={`w-full md:block md:w-auto transition-all duration-300 ease-in-out ${isMenuOpen ? 'menu-animate-enter' : 'hidden'
+                        }`}
+                    id="navbar-default"
+                >
                     <ul className="font-medium flex flex-col p-4 md:p-0 mt-4 border border-gray-100 rounded-lg bg-gray-50 md:flex-row md:space-x-8 rtl:space-x-reverse md:mt-0 md:border-0 md:bg-white">
                         {links.map((link) => (
                             <NavLink
