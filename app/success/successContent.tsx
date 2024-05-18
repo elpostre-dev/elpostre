893f4ec9b2b9ca453956caac02f302be6420f56b
Francisco Guerra <paco.gq.0709@gmail.com>
2024-05-17 19:06:24 -0600

    fix styling success

--- app/success/successContent.tsx
+++ app/success/successContent.tsx
@@ -4,6 +4,7 @@ import { useEffect, useState } from 'react';
 import { useSearchParams } from 'next/navigation';
 import { useCart } from "@/lib/CartContext";
 import { formatCurrency } from '@/lib/utils';
+import Link from 'next/link';
 
 import { productos } from '@/data/productos';
 
@@ -144,7 +145,28 @@ const SuccessContent = () => {
     }
 
     if (!order) {
-        return <div>{errorMessage}</div>;
+        return (<div className="flex flex-col bg-white" style={{ height: '60vh' }}>
+
+            <div className="flex flex-1 items-center justify-center">
+                <div className="mx-auto max-w-xl px-4 text-center">
+                    {/* <h1 className="text-9xl font-extrabold text-gray-500 opacity-50">404</h1> */}
+                    <h1 className="text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl">
+                        Lo sentimos
+                    </h1>
+
+                    <p className="mt-4 text-gray-500">
+                        {errorMessage}
+                    </p>
+
+                    <Link
+                        href="/"
+                        className="mt-6 inline-block rounded bg-mainAmarillo-100 px-5 py-3 text-sm font-medium text-white hover:bg-yellow-700 hover:shadow-lg focus:outline-none focus:ring"
+                    >
+                        Volver al inicio
+                    </Link>
+                </div>
+            </div>
+        </div>)
     }
 
     return (
