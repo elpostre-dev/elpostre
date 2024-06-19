59dbac283271b1818757a6cae5be5a111e580f67
Francisco Guerra <paco.gq.0709@gmail.com>
2024-06-19 15:01:12 -0600

    try fix order updates 8

--- app/admin/page.tsx
+++ app/admin/page.tsx
@@ -4,21 +4,31 @@
 import { SessionProvider } from "next-auth/react";
 import AdminContent from "@/components/AdminContent";
 import useSWR from 'swr';
+import { useEffect, useState } from "react";
+import { set } from "date-fns";
 
 const fetcher = (url: string) => fetch(url).then((res) => res.json());
 
 export default function Admin() {
+    const [AdminContentComponent, setAdminContentComponent] = useState(<></>)
 
     const { data, error } = useSWR('/api/admin/orders', fetcher, {
         refreshInterval: 5000, // Puedes ajustar el tiempo según tus necesidades
     });
+
+    useEffect(() => {
+        console.log('data:', data)
+        if (data) {
+            setAdminContentComponent(<AdminContent orders={data.orders} />)
+        }
+    }, [data])
 
     if (error) return <div>Failed to load orders</div>;
     if (!data) return <div>Loading...</div>;
 
     return (
         <SessionProvider>
-            <AdminContent orders={data.orders} />
+            {AdminContentComponent}
         </SessionProvider>
     );
 }
